5944386b0db8cabaf4339a4cf13915fa4e35017f
Pamela Lin <alemapnil@gmail.com>
2025-12-10 19:07:52 +0800

    alter print

--- members/views.py
+++ members/views.py
@@ -4,11 +4,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    print(request.path,flush=True)
+    print(request.path,'^_^',flush=True)
     return render(request, "index.html")
 
 
 def greet(request):
-    print(request.path,flush=True)
-    print("^_^",flush=True)
+    print(request.path,'!!',flush=True)
     return HttpResponse("Hi ~ Greeting from Django.")
