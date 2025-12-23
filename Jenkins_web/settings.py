42b1a74ef3a190e2e26d3386a6156f13a43e459c
Pamela Lin <alemapnil@gmail.com>
2025-12-23 17:34:05 +0800

    ALLOWED_HOSTS

--- Jenkins_web/settings.py
+++ Jenkins_web/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-03vp^@6&d!!i*=$5p88c@qtpbix_%-ilxyz#i#ef^1ia5+0$if
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
